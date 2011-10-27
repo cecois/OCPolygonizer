998ed6b39dc7c22ff748ad2c003d4b599fc3f046
hahaha jae <jskim326@gmail.com>
2011-10-27 14:33:56 -0500

    implemented the query that identifies all disambiguated entiteis

--- front.php
+++ front.php
@@ -18,13 +18,15 @@ document.getElementById('poly_frame').src='nom.php?geo_name='+geoname+'&hash='+h
   
 <?php
 include "dbh2.php";
-$sql1 = "SELECT * from oc_geo";
+$sql1 = "select count(*) count, ocurl,geonamesho,hash ochash from oc_geo oc left join dblink('dbname=".$osm_db." port=5432 host=localhost user=".$osm_id." password=".$osm_pw."', 'SELECT ochash FROM geogeo') AS s(ochash char(255)) ON oc.hash = s.ochash where the_geom IS NOT NULL AND s.ochash IS NULL group by ocurl,geonamesho,oc.hash";
+
+
 $result1 = pg_query($dbh2, $sql1);
  if (!$result1) {
      die("Error in SQL query: " . pg_last_error());
  } else {
 while ($row1 = pg_fetch_assoc($result1)) {
-echo '<option value="'.$row1['geonamesho'].':::'.$row1['hash'].'">'.$row1['geonamesho'].'</option>';
+echo '<option value="'.$row1['geonamesho'].':::'.$row1['ochash'].'">'.$row1['geonamesho'].'</option>';
 
 
 }
