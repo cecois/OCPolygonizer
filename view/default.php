2ec1f9972e7bf2c2ee2d6419d58f0f9e64cd7976
C.C. Miller <cecmcgee@gmail.com>
2012-04-26 11:16:04 -0400

    removal of obsolete files

--- view/default.php
+++
@@ -1,8 +0,0 @@
-<?php
-$_GET['geo_name']=null;
-$_GET['osm_id']=null;
-$_GET['oid']=null;
-$_GET['hash']=null;
-include 'index.php';
-
-?>
--- view/osm.php
+++
@@ -1,6 +0,0 @@
-<?php
-		$newhtml = str_replace("details.php", "http://localhost/mvc/index.php?osm_id=yes&hash=".$hash."&mode=native&url=http://open.mapquestapi.com/nominatim/v1/details.php", $html);		
-		echo $newhtml;
-
-
-?>
